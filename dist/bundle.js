7786abe54e42eacba6f71d59d726998fe2af0b76
Vasilina Ivanova <lamalaimon@gmail.com>
2022-10-05 23:43:04 +0200

    Folder name change

--- dist/bundle.js
+++ dist/bundle.js
@@ -121899,37 +121899,37 @@ const projects = [
     {
         name: "1",
         id: "1",
-        url: "./IFC/01.ifc"
+        url: "./ifc/01.ifc"
     },
     {
         name: "2",
         id: "2",
-        url: "./IFC/02.ifc"
+        url: "./ifc/02.ifc"
     },
     {
         name: "3",
         id: "3",
-        url: "./IFC/03.ifc"
+        url: "./ifc/03.ifc"
     },
     {
         name: "4",
         id: "4",
-        url: "./IFC/04.ifc"
+        url: "./ifc/04.ifc"
     },
     {
         name: "5",
         id: "5",
-        url: "./IFC/05.ifc"
+        url: "./ifc/05.ifc"
     },
     {
         name: "6",
         id: "1",
-        url: "./IFC/01.ifc"
+        url: "./ifc/01.ifc"
     },
     {
         name: "7",
         id: "1",
-        url: "./IFC/01.ifc"
+        url: "./ifc/01.ifc"
     },
 ];
 
