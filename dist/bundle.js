6186c13f141c5949d6dc65b85a481fb152ffddb1
Vasilina Ivanova <lamalaimon@gmail.com>
2022-10-05 22:02:24 +0200

    Remarks

--- dist/bundle.js
+++ dist/bundle.js
@@ -121947,19 +121947,3 @@ const currentProject = projects.find(project => project.id === currentProjectID)
 
 viewer.IFC.setWasmPath('../');
 viewer.IFC.loadIfcUrl(currentProject.url);
-
-// import { Color } from 'three';
-// import { IfcViewerAPI } from 'web-ifc-viewer';
-
-// const container = document.getElementById('viewer-container');
-// const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
-// viewer.grid.setGrid();
-// viewer.axes.setAxes();
-
-// async function loadIfc(url) {
-//     await viewer.IFC.setWasmPath("../");
-//     const model = await viewer.IFC.loadIfcUrl(url);
-//     viewer.shadowDropper.renderShadow(model.modelID);
-// }
-
-// loadIfc('./ifc/01.ifc');
